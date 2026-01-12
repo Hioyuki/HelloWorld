1d85660b3037a9ebb75e8d6b327e83930ee0188e
Hioyuki <minoruyuki.0214@gmail.com>
2026-01-13 00:03:03 +0900

    Update

--- client.py
+++ client.py
@@ -4,21 +4,20 @@ IP = "127.0.0.1"
 PORT = 9999
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP, PORT))
 
-print("[*] サーバーに接続しました。'exit' と打つと終了します。")
+try:
+    client.connect((IP, PORT))
+    print("[*] Server connected. Type 'exit' to quit.")
 
-while True:
-    # キーボードから入力を受け取る
-    msg = input("メッセージを入力してください >> ")
-    
-    client.send(msg.encode('utf-8'))
-    
-    if msg.lower() == "exit":
-        break
+    while True:
+        msg = input("Message >> ")
+        client.send(msg.encode('utf-8'))
         
-    # サーバーからの返信を表示
-    response = client.recv(1024).decode('utf-8')
-    print(f"[Server]: {response}")
+        if msg.lower() == "exit":
+            break
+            
+        response = client.recv(1024).decode('utf-8')
+        print(f"[Server]: {response}")
 
-client.close()
+finally:
+    client.close()
